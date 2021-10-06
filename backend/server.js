091b9572d93f9d2f3e00540a5bc27e06e6cef23a
Miguel Caballero <cp0251963@gmail.com>
2021-10-06 14:36:16 -0400

    added heroku link to readme

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
 
-app.use(cors());
-app.options('*', cors());
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
+app.use(cors());
+app.options('*', cors());
 
 app.set('clientId', process.env.PAYPAL_CLIENT_ID);
 
